614e8ec63db9f2fcdb83fb503021de03c8e1b9e3
onurdincel <s.onurdincel@gmail.com>
2025-06-13 03:07:20 +0300

    added fps counter

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    dt = 0
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -16,6 +19,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
