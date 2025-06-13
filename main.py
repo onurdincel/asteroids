6a6eeb3ea42b2879b0689af14e4fd4b66db787cb
onurdincel <s.onurdincel@gmail.com>
2025-06-13 04:22:31 +0300

    create player and player movment

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
 
@@ -14,6 +16,10 @@ def main():
 
     while True:
         screen.fill((0, 0, 0))
+
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
 
         for event in pygame.event.get():
---
+++ player.py
@@ -0,0 +1,47 @@
+import pygame
+import circleshape
+import constants
+
+class Player(circleshape.CircleShape):
+
+    def __init__(self, x, y):
+        super().__init__(x, y, constants.PLAYER_RADIUS)
+
+        self.pos_x = x
+        self.pos_y = y
+        self.rotation = 0
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, (255, 255, 255), self.triangle())
+
+    def rotate(self, dt):
+        self.rotation += constants.PLAYER_TURN_SPEED * dt
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0 , 1).rotate(self.rotation)
+        self.position += forward * constants.PLAYER_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
